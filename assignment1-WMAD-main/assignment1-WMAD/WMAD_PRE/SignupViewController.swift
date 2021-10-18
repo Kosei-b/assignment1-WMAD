51e1f773838615eee58e491b3ecf56c569ddd9ed
Kosei <64409793+Kosei-b@users.noreply.github.com>
2021-10-18 16:40:21 -0700

    travel app

---
+++ assignment1-WMAD-main/assignment1-WMAD/WMAD_PRE/SignupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SignupViewController.swift
+//  WMAD_PRE
+//
+//  Created by Kosei Ban on 2021-10-06.
+//
+
+import UIKit
+
+class SignupViewController: UIViewController {
+    
+    var titleValue: String = ""
+    
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        print("value is : \(titleValue)")
+    }
+}
