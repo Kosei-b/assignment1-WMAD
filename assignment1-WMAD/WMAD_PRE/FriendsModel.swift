c0a4e9fc5b152a46d71f2014fa9ae4dbd6367f0d
Kosei <64409793+Kosei-b@users.noreply.github.com>
2021-10-28 01:50:30 -0700

    set friends View Controller!

---
+++ assignment1-WMAD/WMAD_PRE/FriendsModel.swift
@@ -0,0 +1,13 @@
+//
+//  FriendsModel.swift
+//  WMAD_PRE
+//
+//  Created by Kosei Ban on 2021-10-27.
+//
+
+struct FriendModel {
+    var name: String
+    var location: String
+    //var age: Int
+    //var image: String
+}
---
+++ assignment1-WMAD/WMAD_PRE/ViewController.swift
@@ -0,0 +1,99 @@
+//
+//  ViewController.swift
+//  WMAD_PRE
+//
+//  Created by Kosei Ban on 2021-10-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var stuckView: UIStackView!
+    
+    @IBOutlet weak var Login: UIButton!
+    
+    @IBOutlet weak var SignUpButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print( "View Contller veiwdidLoad")
+        
+        //Button Corner Radius
+        Login.layer.cornerRadius = 25
+        SignUpButton.layer.cornerRadius = 25
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        print( "View Contller viewWillAppear")
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        print( "View Contller viewDidAppear")
+        
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        print( "View Contller viewWillDisappear")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+        print( "View Contller viewDidDisappear")
+    }
+    
+    
+    override func performSegue(withIdentifier identifier: String, sender: Any?) {
+        if identifier == "loginSegue"{
+            // do enything related this segue
+        }else if identifier == "siginUpSegue"{
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "loginSegue"{
+            
+            var destinationVC = segue.destination as! LoginViewController; destinationVC.titleValue = "LOGIN TITLE"
+            
+            
+            
+            
+        }else if segue.identifier == "siginUpSegue"{
+            var destinationVC = segue.destination as! SignupViewController; destinationVC.titleValue = "SIGINUP TITLE"
+            
+            
+            
+            
+            
+        }
+        
+    }
+    
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+         
+       // if identifier == "loginSegue"{
+       //     return false
+       // }else {
+        //    return true
+       // }
+        return true
+    }
+    
+    
+    
+
+
+}
+
